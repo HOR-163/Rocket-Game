5ef4119ce41e23449ffb8d637087951f2d6f46e9
HOR-163 <hasse2507@gmail.com>
2024-02-19 17:51:38 +0200

    message

---
+++ Upload git.bat
@@ -0,0 +1,8 @@
+;git init
+;git add .
+set /p message="Enter your commit message:"
+echo message
+;git commit -m message
+;git branch -M main
+;git remote add origin https://github.com/HOR-163/Rocket-Game.git
+;git push -u origin main

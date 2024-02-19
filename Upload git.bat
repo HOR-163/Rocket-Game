4484713ef4f75c5e87c11149057220ecaa5e531b
HOR-163 <hasse2507@gmail.com>
2024-02-19 18:05:37 +0200

    yes

--- Upload git.bat
+++ Upload git.bat
@@ -6,5 +6,6 @@ git commit -m %message%
 git branch -M main
 git remote add origin https://github.com/HOR-163/Rocket-Game.git
 git push -u origin main
-echo \033[32mDone!
+color 0a
+echo Done!
 pause

c9f24af45901a335df9d4702cadae289a3ed1cba
HOR-163 <160273413+HOR-163@users.noreply.github.com>
2024-03-04 15:55:39 +0200

    Update Rocket_game.py
    Added background renderer, which is chunk based
    Tried to add scaler to chunk-based rendering, but as of now it doesn't work (yet!)

--- src/Rocket_game.py
+++ src/Rocket_game.py
@@ -1,12 +1,14 @@
+""" GOOD SOURCES FOR FUTURE """
 # Gravity, physics: https://www.youtube.com/watch?v=5j0uU3aJxJM
 # Camera control: https://www.youtube.com/watch?v=u7LPRqrzry8
 # Collisions: https://www.youtube.com/watch?v=tJiKYMQJnYg
 # Background scrolling/tiling: https://youtu.be/ARt6DLP38-Y
 
+"""Start of the actual program"""
 import pygame, math, os
 import time
 
-#Initialize the pygame module
+#Initialize the pygame module and attributes that are related to it
 pygame.init()
 WIDTH = 1000
 HEIGHT = 750
@@ -18,8 +20,8 @@ timer = pygame.time.Clock()
 
 # load the rocket image
 path = os.getcwd()
-#For IDLE
-#path = path[:path.rfind("\\") + 1]
+# For IDLE uncomment the following line (inVSCode comment this line)
+path = path[:path.rfind("\\") + 1] # <------- This one
 rocket_image_og = pygame.image.load(path + "\\images\\rocket1.png").convert_alpha()
 
 # Set some global variables for the rockets
@@ -42,7 +44,7 @@ class Rocket:
         self.x_speed = 0
         self.y_speed = 0
         
-
+    """ Keyboard input handling and accelerationa and speed physics """
     def input(self):
         keys = pygame.key.get_pressed()
 
@@ -68,32 +70,64 @@ class Rocket:
 
         #Pressing the "R" key, resets everything
         if keys[pygame.K_r]:
-            self.x_pos = 500
-            self.y_pos = 375
+            self.x_pos = WIDTH / 2
+            self.y_pos = HEIGHT / 2
             self.x_speed = 0
             self.y_speed = 0
             self.angle = 0
-        
+
+    """ Update the rockets position """
     def update(self):
         self.input()
         self.x_pos -= self.x_speed
         self.y_pos += self.y_speed
 
         # Put the image in the centre of our imaginary "position point" aka when rotating the rocket
-        # it rotates from the center, not form the upper left corenr of the image (sprite)
+        # it rotates from the center, not from the upper left corenr of the image (sprite)
+        # Always draw the rocket in the center of the screen (maybe change later)
         rocket_image = pygame.transform.rotate(rocket_image_og, self.angle)
-        rocket_image_rect = rocket_image.get_rect(center = (self.x_pos, self.y_pos))
+        rocket_image_rect = rocket_image.get_rect(center = (WIDTH/2, HEIGHT/2))
         screen.blit(rocket_image, rocket_image_rect)
+        #pygame.draw.circle(screen, 'red', (500,375), 1)
 
+"""Handle background chunks"""
+class Background:
+    def __init__(self, load_x, load_y):
+        # how many chunks to load (e.g 3x3 amount of chunks)
+        self.load_x = load_x
+        self.load_y = load_y
+        
+    def update(self, x_pos, y_pos):
+        # x and y positions are reversed
+        scale_x = 1
+        scale_y = 1
+        chunk_y = -y_pos // (HEIGHT * scale_x) + 1
+        chunk_x = x_pos // (WIDTH * scale_y)
+
+        for x in range(self.load_x):
+            for y in range (self.load_y):
+                # Draw chunk borders 
+                #screen.blit(background_image, ((chunk_x + x - self.load_x // 2) * WIDTH - x_pos - WIDTH // 2,
+                                               #(-chunk_y + y -self.load_y //2) * HEIGHT - y_pos + HEIGHT // 2))
+                pygame.draw.rect(screen, 'black', pygame.Rect(
+                                                (chunk_x + x + 1 - self.load_x // 2) * WIDTH * 1 - x_pos - WIDTH // 2, # box's top left corners x-coordinate
+                                                (-chunk_y + y - self.load_y // 2) * HEIGHT * 1 - y_pos + HEIGHT // 2, # box's top left corners y-coordinate
+                                                WIDTH * scale_x, HEIGHT * scale_y), # Box size
+                                1) # Border width
+                # Draw chunk texts
+                chunk_data = font.render(f"{int(chunk_x)} {int(chunk_y)}", True, 'black', 'white')
+                screen.blit(chunk_data, (int(chunk_x + x - self.load_x // 2) * WIDTH - x_pos, (-chunk_y + y -self.load_y //2) * HEIGHT - y_pos))
+        
+        
 
 rocket1 = Rocket(500, 375, 0.1, 0.2, 1)
-
+background = Background(3,3)
 
 
 first = True
 def print_debug_info(last_speed_x, last_speed_y):
-    x_position = round(rocket1.x_pos - 500, 3)
-    y_position = round(-rocket1.y_pos + 375, 3)
+    x_position = round(rocket1.x_pos, 3)
+    y_position = round(rocket1.y_pos, 3)
     x_speed = round(rocket1.x_speed, 3)
     y_speed = round(rocket1.y_speed, 3)
     x_acc = round((x_speed - last_speed_x), 3)
@@ -101,6 +135,7 @@ def print_debug_info(last_speed_x, last_speed_y):
     angle = round(rocket1.angle, 3)
     texts = [font.render(f"x-position: {x_position}", True, 'red', 'white'),
              font.render(f"y-position: {y_position}", True, 'blue', 'white'),
+             font.render(f"Chunk based position: {int(rocket1.x_pos // WIDTH + 1)} {int(-rocket1.y_pos // HEIGHT + 1)}", True, 'black', 'white'),
              font.render(f"x-speed: {-x_speed}", True, 'red', 'white'),
              font.render(f"y-speed: {-y_speed}", True, 'blue', 'white'),
              font.render(f"Actual speed: {round(math.sqrt(x_speed**2 + y_speed**2),3)}", True, 'black', 'white'),
@@ -110,7 +145,7 @@ def print_debug_info(last_speed_x, last_speed_y):
              font.render(f"FPS: {round(1000000000/(deltatime), 3)}", True, 'black', 'white')
              ]
     for index, text in enumerate(texts):
-        screen.blit(text,(0,index * 16))
+        screen.blit(text, (0, index * 16))
 
 
 
@@ -134,6 +169,7 @@ while run:
     screen.fill(background_color)
 
     # Run the main code in the rocket object
+    background.update(rocket1.x_pos, rocket1.y_pos)
     rocket1.update()
 
     # Read hid inputs
